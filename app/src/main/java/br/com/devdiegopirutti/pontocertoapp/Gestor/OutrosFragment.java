fe06d9ef48d815b9af036b835d465758d71331b6
Yuri Orfon <yuriorfon@hotmail.com>
2020-05-16 23:23:34 -0300

    Adição da activity gestor e fragments

---
+++ app/src/main/java/br/com/devdiegopirutti/pontocertoapp/Gestor/OutrosFragment.java
@@ -0,0 +1,21 @@
+package br.com.devdiegopirutti.pontocertoapp.Gestor;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import br.com.devdiegopirutti.pontocertoapp.R;
+
+public class OutrosFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_outros, null);
+
+    }
+}
