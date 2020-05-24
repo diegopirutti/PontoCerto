c57d0f465802ef72794b4bfd72f1d9e685a231e4
Diego Pirutti <diego.pirutti93@gmail.com>
2020-05-24 19:32:12 -0300

    recyclerview arrumado

--- app/src/main/java/br/com/devdiegopirutti/pontocertoapp/Gestor/AdapterColaboradores.kt
+++ app/src/main/java/br/com/devdiegopirutti/pontocertoapp/Gestor/AdapterColaboradores.kt
@@ -1,18 +1,20 @@
 package br.com.devdiegopirutti.pontocertoapp.Gestor
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import br.com.devdiegopirutti.pontocertoapp.Model.UsersToGestor
+import br.com.devdiegopirutti.pontocertoapp.R
 
 
 class AdapterColaboradores(var arrayList: ArrayList<UsersToGestor>, var context: Context) : RecyclerView.Adapter<AdapterColaboradores.ViewHolderColaboradores>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderColaboradores {
-//         val view = LayoutInflater.from(context).inflate(R.layout.user_item, parent, false)
-//        return ViewHolderColaboradores(view)
+         val view = LayoutInflater.from(context).inflate(R.layout.user_item, parent, false)
+        return ViewHolderColaboradores(view)
     }
 
     override fun getItemCount(): Int = arrayList.size
